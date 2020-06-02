2d7d880febb642a87216ebc40d8ab37dff04f819
TiLied <TiLied@users.noreply.github.com>
2020-06-03 01:13:16 +0300

    0.2.00
    Added one more api http://api.lololyrics.com/.
    Added ability edit lyrics.
    Added options:
    debug,
    context menu(RightClickMouse on anything at page, you can change to return that context menu),
    priority(you can change priority of using apis, for example if you listen a lot of electronic music I suggest putting api-http://api.lololyrics.com/ at the top, right under "Local cache"),
    Later will change description with screenshots. Have fun listening music with lyrics! :)

--- Youtube_Music_Lyrics.user.js
+++ Youtube_Music_Lyrics.user.js
@@ -4,8 +4,9 @@
 // @description Adds lyrics to Youtube Music 
 // @include     https://music.youtube.com/*
 // @require     https://code.jquery.com/jquery-3.5.1.min.js
+// @require     https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
 // @author      TiLied
-// @version     0.1.01
+// @version     0.2.00
 // @grant       GM_listValues
 // @grant       GM_getValue
 // @grant       GM_setValue
@@ -25,9 +26,13 @@ const oneSecond = 1000,
 	oneMonth = oneWeek * 4;
 
 var cache = {},
-	tTitle = 0;
+	tTitle = 0,
+	options;
 
-var debug = false;
+var debug,
+	contextmenu,
+	ret,
+	priority;
 
 //Start
 //Function main
@@ -37,16 +42,16 @@ void function Main()
 	//Place CSS in head
 	SetCSS();
 
-	//Disable secodn mouse click(contextmenu)
-	//TODO make option
-	document.addEventListener("contextmenu", function (e) { e.button === 2 && e.stopPropagation(); }, true);
-
 	//Url handler for changing
 	UrlHandler();
 
 	//Set settings or create
 	SetSettings(function ()
 	{
+		//Disable second mouse click(contextmenu)
+		if (contextmenu)
+			document.addEventListener("contextmenu", function (e) { e.button === 2 && e.stopPropagation(); }, true);
+
 		//Check music.
 		setTimeout(function ()
 		{
@@ -64,18 +69,207 @@ void function Main()
 //End
 
 //Start
+//Class options
+class Options
+{
+	constructor(debug, contextmenu, priority)
+	{
+		this.debug = debug;
+		this.contextmenu = contextmenu;
+		this.priority = priority;
+	}
+
+	get values()
+	{
+		let v = 
+		{
+			debug: this.debug,
+			contextmenu: this.contextmenu,
+			priority: this.priority
+		};
+		return v;
+	}
+
+	static string(s)
+	{
+		switch (s) { 
+			case "one":
+				return "api - https://github.com/NTag/lyrics.ovh";
+			case "two":
+				return "api - https://github.com/rhnvrm/lyric-api";
+			case "three":
+				return "api - http://api.lololyrics.com/";
+		}
+	}
+
+	set values(obj)
+	{
+		this.debug = obj["debug"];
+		this.contextmenu = obj["contextmenu"];
+		this.priority = obj["priority"];
+	}
+
+	priorityF(p, id, artist, title)
+	{
+		let lyrics;
+
+		switch (p)
+		{
+			case "zero":
+				{
+					return new Promise(function (resolve, reject)
+					{
+						if ($.isEmptyObject(cache[id]) || typeof cache[id]["musics"][title] === "undefined")
+						{
+							resolve(false);
+						} else
+						{
+							AddCache(id, artist, title, lyrics);
+							DisplayLyrics(id, title);
+							resolve(true);
+						}
+					});
+				}
+			case "one":
+				{
+					return new Promise(function (resolve, reject)
+					{
+					//api - https://github.com/NTag/lyrics.ovh
+					GM.xmlHttpRequest({
+							method: "GET",
+							url: "https://api.lyrics.ovh/v1/" + artist + "/" + title + "",
+							timeout: oneSecond * 5,
+							onload: function (response)
+							{
+								if (debug) console.log(response);
+								let r = JSON.parse(response.responseText);
+								if (Object.keys(r)[0] === "lyrics")
+								{
+									lyrics = r["lyrics"];
+									if (debug) console.log(lyrics);
+									AddCache(id, artist, title, lyrics);
+									DisplayLyrics(id, title);
+									resolve(true);
+								} else
+								{
+									if (debug) console.log(r);
+									resolve(false);
+								}
+							},
+							onerror: function (e)
+							{
+								console.error(e);
+								resolve(false);
+							}
+							});
+					});
+				}
+			case "two":
+				{
+					return new Promise(function (resolve, reject)
+					{
+					//api - https://github.com/rhnvrm/lyric-api
+
+			GM.xmlHttpRequest({
+				method: "GET",
+				url: "https://lyric-api.herokuapp.com/api/find/" + artist + "/" + title + "",
+				timeout: oneSecond * 5,
+				onload: function (response)
+				{
+					if (debug) console.log(response);
+					let r = JSON.parse(response.responseText);
+					if (r["err"] === "none")
+					{
+						lyrics = r["lyric"];
+						if (debug) console.log(lyrics);
+						AddCache(id, artist, title, lyrics);
+						DisplayLyrics(id, title);
+						resolve(true);
+					} else
+					{
+						if (debug) console.log(r);
+						resolve(false);
+					}
+				},
+				onerror: function (e)
+				{
+					console.error(e);
+					resolve(false);
+				}
+			});
+					});
+				}
+			case "three":
+				{
+					return new Promise(function (resolve, reject)
+					{
+					//api - http://api.lololyrics.com/
+
+			GM.xmlHttpRequest({
+				method: "GET",
+				url: "http://api.lololyrics.com/0.5/getLyric?artist=" + artist + "&track=" + title + "",
+				timeout: oneSecond * 5,
+				onload: function (response)
+				{
+					if (debug) console.log(response);
+					let xml = response.responseXML.all;
+
+					if (xml[1].textContent === "OK")
+					{
+						lyrics = xml[2].innerHTML;
+						if (debug) console.log(lyrics);
+						AddCache(id, artist, title, lyrics);
+						DisplayLyrics(id, title);
+						resolve(true);
+					} else
+					{
+						if (debug) console.log(xml);
+						resolve(false);
+					}
+				},
+				onerror: function (e)
+				{
+					console.error(e);
+					resolve(false);
+				}
+			});
+					});
+				}
+			case "last":
+				{
+					return new Promise(function (resolve, reject)
+					{
+						if (debug) console.log("No lyrics found!");
+						DisplayLyrics(id, title, "No lyrics found!");
+						if (debug) console.log(priority);
+						resolve(true);
+					});
+				}
+		}
+	}
+}
+//Class options
+//End
+
+//Start
 //Functions GM_VALUE
 async function SetSettings(callBack)
 {
 	try
 	{
-		//DeleteValues("all");
+		//DeleteValues("yml_options");
 		//THIS IS ABOUT OPTIONS
-		//if (await HasValue("yml_options", JSON.stringify(options)))
-		//{
-		//	options = JSON.parse(await GM.getValue("yml_options"));
-		//	SetOptionsObj();
-		//}
+		if (await HasValue("yml_options", JSON.stringify(options)))
+		{
+			let v = JSON.parse(await GM.getValue("yml_options"));
+			options = new Options(false, true, ["zero", "one", "two", "three", "last"]);
+			options.values = v;
+			SetOptionsObj();
+		} else
+		{
+			options = new Options(false, true, ["zero", "one", "two", "three", "last"]);
+			SetOptionsObj();
+		}
 
 		//THIS IS ABOUT CACHE
 		if (await HasValue("yml_cache", JSON.stringify(cache)))
@@ -97,7 +291,7 @@ async function SetSettings(callBack)
 		console.log("*-----*");
 		if (debug)
 		{
-			//console.log(options);
+			console.log(options);
 			console.log(cache);
 		}
 
@@ -199,7 +393,7 @@ function UpdateGM(what)
 			GM.setValue("yml_cache", gmVal);
 			break;
 		case "options":
-			gmVal = JSON.stringify(options);
+			gmVal = JSON.stringify(options.values);
 			GM.setValue("yml_options", gmVal);
 			break;
 		default:
@@ -214,7 +408,9 @@ function UpdateGM(what)
 //Functions create object option and cache
 function SetOptionsObj()
 {
-
+	debug = options.debug;
+	contextmenu = options.contextmenu;
+	UpdateGM("options");
 }
 
 function SetCacheObj()
@@ -244,7 +440,7 @@ function SetCacheObj()
 						delete cache[prop];
 					}
 				}
-				DeleteValues("yml_cache");
+				//DeleteValues("yml_cache");
 			}
 		}
 	} catch (e) { console.error(e); }
@@ -256,7 +452,7 @@ function SetCacheObj()
 //Functions Get music
 function Music()
 {
-	var priority = 0;
+	priority = options.priority;
 
 	var artist = document.querySelector(".subtitle.ytmusic-player-bar").firstElementChild.firstElementChild.innerText;
 	var title = document.querySelector(".title.ytmusic-player-bar").innerText;
@@ -287,6 +483,45 @@ function Music()
 //Function Add to Cache lyrics
 async function AddCache(id, artist, title, lyrics)
 {
+	if (typeof title === "undefined")
+	{
+		//TODO DO SOMETHING 
+		var a = document.querySelector(".subtitle.ytmusic-player-bar").firstElementChild.firstElementChild.innerText;
+		var t = document.querySelector(".title.ytmusic-player-bar").innerText;
+		var i = document.querySelector(".subtitle.ytmusic-player-bar").firstElementChild.firstElementChild.attributes.href.value;
+
+		if ($.isEmptyObject(cache[i]))
+		{
+			cache[i] =
+			{
+				name: a,
+				musics: {},
+				//statistics
+				gettingLyricsForArtistTimes: 1,
+				custom: ""
+			};
+		}
+
+		if (typeof cache[i]["musics"][t] === "undefined")
+		{
+
+			cache[i]["musics"][t] =
+			{
+				title: t,
+				lyrics: lyrics,
+				dateId: Date.now(),
+				//statistics
+				gettingLyricsForMusicTimes: 1,
+				custom: ""
+			};
+
+		} else
+		{
+			cache[i]["musics"][t]["lyrics"] = lyrics;
+		}
+		return;
+	}
+
 	if ($.isEmptyObject(cache[id]))
 	{
 		cache[id] =
@@ -331,93 +566,12 @@ async function AddCache(id, artist, title, lyrics)
 
 //Start
 //Function xml api
-function GetLyrics(priority, id, artist, title)
+async function GetLyrics(priority, id, artist, title)
 {
-	var lyrics;
-
-	switch (priority)
+	for (let i = 0; priority.length; i++)
 	{
-		case 0:
-			if ($.isEmptyObject(cache[id]) || typeof cache[id]["musics"][title] === "undefined")
-			{
-				priority++;
-				return GetLyrics(priority, id, artist, title);
-			} else
-			{
-				AddCache(id, artist, title, lyrics);
-				DisplayLyrics(id, title);
-			}
-			break;
-		case 1:
-			//api - https://github.com/NTag/lyrics.ovh
-			if (debug) console.log(priority);
-
-			GM.xmlHttpRequest({
-				method: "GET",
-				url: "https://api.lyrics.ovh/v1/" + artist + "/" + title + "",
-				timeout: oneSecond * 5,
-				onload: function (response)
-				{
-					if (debug) console.log(response);
-					let r = JSON.parse(response.responseText);
-					if (Object.keys(r)[0] === "lyrics")
-					{
-						lyrics = r["lyrics"];
-						if (debug) console.log(lyrics);
-						AddCache(id, artist, title, lyrics);
-						DisplayLyrics(id, title);
-					} else
-					{
-						if (debug) console.log(r);
-						priority++;
-						return GetLyrics(priority, id, artist, title);
-					}
-				},
-				onerror: function (e)
-				{
-					console.error(e);
-					priority++;
-					return GetLyrics(priority, id, artist, title);
-				}
-			});
-			break;
-		case 2:
-			//api - https://github.com/rhnvrm/lyric-api
-			if (debug) console.log(priority);
-
-			GM.xmlHttpRequest({
-				method: "GET",
-				url: "https://lyric-api.herokuapp.com/api/find/" + artist + "/" + title + "",
-				timeout: oneSecond * 5,
-				onload: function (response)
-				{
-					if (debug) console.log(response);
-					let r = JSON.parse(response.responseText);
-					if (Object.keys(r)[0] === "lyric" && r[1] === "none")
-					{
-						lyrics = r["lyric"];
-						if (debug) console.log(lyrics);
-						AddCache(id, artist, title, lyrics);
-						DisplayLyrics(id, title);
-					} else
-					{
-						if (debug) console.log(r);
-						priority++;
-						return GetLyrics(priority, id, artist, title);
-					}
-				},
-				onerror: function (e)
-				{
-					console.error(e);
-					priority++;
-					return GetLyrics(priority, id, artist, title);
-				}
-			});
-			break;
-		default:
-			if (debug) console.log("No lyrics found!");
-			DisplayLyrics(id, title, "No lyrics found!");
-			if (debug) console.log(priority);
+		let aww = await options.priorityF(priority[i], id, artist, title);
+		if(aww === true)
 			break;
 	}
 
@@ -435,8 +589,23 @@ function SetUI()
 {
 	var rightC = $(".middle-controls-buttons");
 	var mainP = $("#main-panel");
-	var divP = $("<div id=yml_lyricsPanel class='style-scope ytmusic-player-page'></div>").html("<pre class='style-scope ytmusic-player-baryt-formatted-string' style='color:inherit; font-family:inherit; padding-left:5%;'>Lyrics:</pre>");
-	var divB = $("<div id=yml_lyricsButton class='right-controls-buttons style-scope ytmusic-player-bar'></div>").html("<a class='yt-simple-endpoint style-scope ytmusic-player-bar yt-formatted-string' style='color:inherit;'>Lyrics</a>");
+	var divP = $("<div id=yml_lyricsPanel class='style-scope ytmusic-player-page'></div>").html("<pre id=yml_lyricsText class='style-scope ytmusic-player-baryt-formatted-string'>Lyrics:</pre>");
+	var divB = $("<div id=yml_lyricsButton class='right-controls-buttons style-scope ytmusic-player-bar'></div>").html("<a class='yml_Button style-scope ytmusic-player-bar yt-formatted-string' style='color:inherit;'>Lyrics</a>");
+
+	var divPB = $("<div id=yml_PanelButtons class='style-scope ytmusic-player-page'></div>").html("<a class='yml_Button' id=yml_addLyricsButton>Add lyrics</a><a class='yml_Button' id=yml_optionButton style='padding-left: 10px;'>Options</a>");
+	var divPO = $("<div id=yml_optionsPanel class='style-scope ytmusic-player-page'></div>").html("<a class='style-scope ytmusic-player-baryt-formatted-string' style='color:inherit; font-family:inherit;'>Options:</a><form>\
+<br>\
+	<input type=checkbox name=debug id=yml_debug >Debug</input><br> \
+	<input type=checkbox name=contextmenu id=yml_contextmenu >Context menu</input><br> \
+<ul id='image-list1' class='sortable-list'>\
+		<br>\
+	<li class='ui-state-default ui-state-disabled' id='zero'>Locale cache</li>\
+	<li class='ui-state-default' id='one'><span>api-https://github.com/NTag/lyrics.ovh</span></li>\
+	<li class='ui-state-default' id='two'><span>api-https://github.com/rhnvrm/lyric-api</span></li>\
+	<li class='ui-state-default' id='three'><span>api-http://api.lololyrics.com/</span></li>\
+	<li class='ui-state-default ui-state-disabled' id='last' style='display:none;'>Last Not Found!</li>\
+</ul >\
+</form>");
 
 	if (debug)
 	{
@@ -444,11 +613,32 @@ function SetUI()
 		console.log(mainP);
 	}
 
+	$(divP).append(divPB);
+	$(divP).prepend(divPO);
 	$(mainP).append(divP);
 	$(rightC).append(divB);
 	$(divP).hide();
+	$(divPO).hide();
+	UIValues();
 }
 //Function set ui 
+//End
+
+
+//Start
+//Function set UI values of settengs/options
+function UIValues()
+{
+	$("#yml_debug").prop("checked", debug);
+	$("#yml_contextmenu").prop("checked", contextmenu);
+	let li = $(".sortable-list li");
+	for (let i = 1; i < options.priority.length; i++)
+	{
+		$(li[i]).attr("id", options.priority[i]);
+		$(li[i]).find("span").text(Options.string(options.priority[i]));
+	}
+}
+//Function set events
 //End
 
 //Start
@@ -466,10 +656,43 @@ function SetCSS()
 		color:#aaaaaa;\
 }"));
 
-	$("head").append($("<style type=text/css></style>").text("#yml_lyricsButton { \
+	$("head").append($("<style type=text/css></style>").text("#yml_lyricsText { \
+	color: inherit;\
+	font-family: inherit;\
+	padding-left: 5%;\
+}"));
+
+	$("head").append($("<style type=text/css></style>").text("#yml_PanelButtons { \
+	float: right;\
+	padding:10px;\
+	margin:10px;\
+	border: 3px solid;\
+}"));
+
+	$("head").append($("<style type=text/css></style>").text("#yml_optionsPanel { \
+		position: fixed;\
+		z-index: 111;\
+		padding-left: 5%;\
+		padding-bottom: 5%;\
+		border: 3px solid;\
+		background-color: #2d2d2d;\
+		font-size:16px;\
+		overflow-y:scroll;\
+		color:#aaaaaa;\
+}"));
+
+	$("head").append($("<style type=text/css></style>").text(".yml_Button { \
 	cursor: pointer;\
 		font-size:16px;\
 		color:#aaaaaa;\
+}"));
+
+	$("head").append($("<style type=text/css></style>").text(".yml_Button:hover { \
+	text-decoration: underline;\
+}"));
+
+	$("head").append($("<style type=text/css></style>").text(".ui-state-default { \
+	cursor: move;\
 }"));
 
 	$("head").append($("<!--End of Youtube Music Lyrics v" + GM.info.script.version + " CSS-->"));
@@ -507,6 +730,76 @@ function SetEvents()
 			style: "max-height: " + h + "px;max-width:" + (w + 100) + "px;min-width:" + w + "px;"
 		});
 	});
+
+	$("#yml_optionButton").click(function ()
+	{
+		$("#yml_optionsPanel").toggle();
+		if ($("#yml_optionsPanel").css('display') !== 'none')
+		{
+			$(this).text("Save Options");
+		} else
+		{
+			UpdateGM("options");
+			$(this).text("Options");
+		}
+	});
+
+	$("#yml_debug").change(function ()
+	{
+		options.debug = $(this).prop("checked");
+		debug = $(this).prop("checked");
+	});
+
+	$("#yml_contextmenu").change(function ()
+	{
+		options.contextmenu = $(this).prop("checked");
+		debug = $(this).prop("checked");
+	});
+
+	$("#yml_addLyricsButton").click(function ()
+	{
+		//TODO MAKE BETTER CODE!!!
+		if (debug) console.log($("#yml_lyricsText").prop("tagName"));
+		if ($("#yml_lyricsText").prop("tagName") === "PRE")
+		{
+			$(this).text("Save Lyrics");
+			let w = $("#yml_lyricsText").width();
+			let h = $("#yml_lyricsText").height();
+			$("#yml_lyricsText").attr({
+				style: "height: " + h + "px;width:" + (w + 100) + "px;"
+			});
+			$('pre#yml_lyricsText').replaceTag('textarea');
+			document.getElementById("yml_lyricsText").value($("#yml_lyricsText").text());
+		}
+		else
+		{
+			$(this).text("Add Lyrics");
+			$("#yml_lyricsText").text(document.getElementById("yml_lyricsText").value);
+			//console.log(document.getElementById("yml_lyricsText").value);
+			//cache[id]["musics"][title]["lyrics"] = document.getElementById("yml_lyricsText").value;
+			AddCache(undefined, undefined, undefined, document.getElementById("yml_lyricsText").value);
+			UpdateGM("cache");
+			$("#yml_lyricsText").attr({
+				style: "height: inherit;width:inherit;"
+			});
+			$('textarea#yml_lyricsText').replaceTag('pre');
+		}
+	});
+
+	$('.sortable-list').sortable({
+		connectWith: '.sortable-list',
+		items: "li:not(.ui-state-disabled)",
+		update: function (event, ui)
+		{
+			var order = $(this).sortable('toArray');
+			order.unshift("zero");
+			order.push("last");
+			console.log(order);
+			options.priority = order;
+			priority = order;
+		}
+	});
+
 }
 //Function set events
 //End
@@ -534,4 +827,25 @@ function UrlHandler()
 	this.Check = setInterval(function () { detect(); }, 200);
 }
 //Handler for url
+//End
+
+//Start
+//Tool for changing tags https://stackoverflow.com/a/32067355
+(function ($)
+{
+	$.fn.replaceTag = function (newTag)
+	{
+		var originalElement = this[0]
+			, originalTag = originalElement.tagName
+			, startRX = new RegExp('^<' + originalTag, 'i')
+			, endRX = new RegExp(originalTag + '>$', 'i')
+			, startSubst = '<' + newTag
+			, endSubst = newTag + '>'
+			, newHTML = originalElement.outerHTML
+				.replace(startRX, startSubst)
+				.replace(endRX, endSubst);
+		this.replaceWith(newHTML);
+	};
+})(jQuery);
+//Tool for changing tags https://stackoverflow.com/a/32067355
 //End
